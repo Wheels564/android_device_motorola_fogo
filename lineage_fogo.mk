21a7f41c99b5b2197673ae29e85fcee9ed73d9aa
Michael Bestas <mkbestas@lineageos.org>
2026-07-25 20:48:16 +0300

    fogo: Update to V1UFNS35H.193-20-14
    Change-Id: Ib039fffa6b29e146a23a4313bc474d719b24a515

--- lineage_fogo.mk
+++ lineage_fogo.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := moto g 5G - 2024
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogo_g-user 15 V1UFNS35H.193-20-12 66040b-a80b154 release-keys MV-304" \
-    BuildFingerprint=motorola/fogo_g/fogo:15/V1UFNS35H.193-20-12/66040b-a80b154:user/release-keys \
+    BuildDesc="fogo_g-user 15 V1UFNS35H.193-20-14 2c2db8-05b2f release-keys MV-304" \
+    BuildFingerprint=motorola/fogo_g/fogo:15/V1UFNS35H.193-20-14/2c2db8-05b2f:user/release-keys \
     DeviceProduct=fogo_g
